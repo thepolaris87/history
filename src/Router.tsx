1939dcd472b7d24c90da4503a8e01537e120dcfb
김진범 <polaris@esls.io>
2021-12-03 17:27:24 +0900

    update

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,19 +1,23 @@
-import { Route, Routes } from 'react-router';
+import { Outlet, Route, Routes } from 'react-router';
 import { Link } from 'react-router-dom';
 
 export default function Router() {
+    console.log('v1');
+
     return (
         <>
             <Routes>
-                <Route
-                    path=''
-                    element={
-                        <div>
-                            <Link to='/test'>TEST로</Link>
-                        </div>
-                    }
-                />
-                <Route path='/test' element={<div>TEST</div>} />
+                <Route path='/history/dist' element={<Outlet />}>
+                    <Route
+                        index
+                        element={
+                            <div>
+                                <Link to='test'>TEST로</Link>
+                            </div>
+                        }
+                    />
+                    <Route path='test' element={<div>TEST</div>} />
+                </Route>
             </Routes>
         </>
     );
