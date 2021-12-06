03ac6c6ac26e9a35a35ad40646bec292d79309fa
김진범 <polaris@esls.io>
2021-12-06 09:12:53 +0900

    update

--- src/slices/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import test from './test';
-
-const rootReducer = combineReducers({ test });
-
-export type RootState = ReturnType<typeof rootReducer>;
-
-export default rootReducer;
